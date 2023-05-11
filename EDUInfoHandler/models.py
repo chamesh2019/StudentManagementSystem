cb25f4fd403ccbb472f31bdc4d9b15557b3f88c5
= <chamesh2020@gmail.com>
2023-05-11 21:24:55 +0530

    finall bug fix

--- EDUInfoHandler/models.py
+++ EDUInfoHandler/models.py
@@ -51,7 +51,7 @@ class LoginKey(models.Model):
     key = models.CharField(max_length=255)
     date = models.CharField(max_length=20)
     acc_type = models.CharField(max_length=25)
-    identifier = models.IntegerField(default=0)
+    identifier = models.CharField(max_length=25)
     
     def __str__(self) -> str:
         return str(self.identifier)
--- TeacherInfoHandler/urls.py
+++ TeacherInfoHandler/urls.py
@@ -5,7 +5,7 @@ from .views import TeacherAdd, TeacherView, TeacherListView, MarksAdd
 
 urlpatterns = [
     path("add/", TeacherAdd.as_view(), name="TeacherAdd"),
-    path("<int:nic>/", TeacherView.as_view(), name="TeacherView"),
+    path("<str:nic>/", TeacherView.as_view(), name="TeacherView"),
     path("", TeacherListView.as_view(), name="TeacherListView"),
     path("addmarks/<int:grade>/<int:subject>", MarksAdd.as_view(), name="AddMarks")
 ]
